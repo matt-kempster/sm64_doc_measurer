837b9b1cee5f5eb170fca52b4cd0596e1fa7dd96
Matt Kempster <kidpixel@gmail.com>
2019-08-06 20:22:34 -0700

    Move static site to gitignore and improve generation

--- generate_site.py
+++ generate_site.py
@@ -5,6 +5,7 @@ import socketserver
 from pathlib import Path
 
 from index import render
+
 # The following terrible wildcard import is necessitated by my local pickle cache.
 # See https://stackoverflow.com/q/27732354/11799075. I'll have to go back and
 # fix this later.
@@ -12,21 +13,39 @@ from sm64_parse import *
 
 PORT = 8000
 
-if __name__ == "__main__":
+import shutil
+
+
+def serve_site(site_output: Path, port: int) -> None:
+    os.chdir(site_output)
+    with socketserver.TCPServer(
+        ("", port), http.server.SimpleHTTPRequestHandler
+    ) as httpd:
+        print("serving at port", port)
+        httpd.serve_forever()
+
+
+def main_site() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("root", metavar="PATH_TO_SM64_SOURCE_DIR")
     args = parser.parse_args()
+
     root = Path(args.root)
     results = sm64_parse(root, commits_to_analyze=50)
 
-    site_output = Path(__file__).parent / "_site"
+    parent = Path(__file__).parent
+    site_output = parent / "_site"
+
     site_output.mkdir(exist_ok=True)
-
     (site_output / "index.html").write_text(render({"results": results}))
+    if not (site_output / "js").is_dir():
+        shutil.copytree(parent / "js", site_output / "js")
+    if not (site_output / "graph.css").is_file():
+        shutil.copy(parent / "graph.css", site_output / "graph.css")
 
-    Handler = http.server.SimpleHTTPRequestHandler
+    serve_site(site_output, PORT)
+    return 0
 
-    os.chdir(site_output)
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        httpd.serve_forever()
+
+if __name__ == "__main__":
+    sys.exit(main_site())
